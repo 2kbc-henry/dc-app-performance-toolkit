77d7aca838f76f0febafc1588d80bb560f7b063d
Henry Pitman <henry.pitman@2000bc.io>
2021-03-14 18:48:05 +0100

    perf(linkboard): dev setup for linkboard

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -9,16 +9,16 @@ from util.conf import JIRA_SETTINGS
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    if datasets['project_key']:
+        project_key = datasets['project_key']
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
         @print_timing("selenium_app_custom_action:view_issue")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{project_key}?selectedItem=linkboard:linkboard-page")
+            page.wait_until_visible((By.ID, "lb-root"))  # Wait for summary field visible
+            page.wait_until_visible((By.CSS_SELECTOR, "#lb-root .ijnXzl"))  # Wait for summary field visible
         sub_measure()
     measure()
